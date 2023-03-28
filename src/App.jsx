689f22bc5b0d1306fb05e82ce5c4e17863a64445
Claudia Lorien <claudia.calero@ultimasoftware.com>
2023-03-28 13:27:46 +0200

    controlled form and validations

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import './App.css'
+import { useState } from 'react'
 import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
+import { useEffect } from 'react'
 
 function App() {
   const { movies } = useMovies()
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const fields = new window.FormData(event.target)
-    const { query } = Object.fromEntries(fields)
     console.log({ query })
   }
+
+  const handleChange = (event) => {
+    const newQuery = event.target.value
+    if (newQuery.startsWith(' ')) return
+    setQuery(newQuery)
+  }
+
+  useEffect(() => {
+    if (query === '') {
+      setError('No se puede buscar una película si el input está vacío')
+      return
+    }
+    if (query.length < 3) {
+      setError('La película debe tener al menos 3 caracteres')
+      return
+    }
+    setError(null)
+  }, [query])
 
   return (
     <div className="page">
@@ -18,11 +38,14 @@ function App() {
         <h1>Buscador de películas</h1>
         <form className="form" onSubmit={handleSubmit}>
           <input
+            onChange={handleChange}
+            value={query}
             name={'query'}
             placeholder="Avengers, Star Wars, The Matrix..."
           />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies} />
