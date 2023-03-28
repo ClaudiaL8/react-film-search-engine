4f190963168c71fa72f2f61f001a3909ae5474ee
Claudia Lorien <claudia.calero@ultimasoftware.com>
2023-03-28 13:11:16 +0200

    uncontrolled form

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,22 @@ import { Movies } from './components/Movies'
 function App() {
   const { movies } = useMovies()
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const fields = new window.FormData(event.target)
+    const { query } = Object.fromEntries(fields)
+    console.log({ query })
+  }
+
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form className="form">
-          <input placeholder="Avengers, Star Wars, The Matrix..." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            name={'query'}
+            placeholder="Avengers, Star Wars, The Matrix..."
+          />
           <button type="submit">Buscar</button>
         </form>
       </header>
